ac6811b2ae0f42093575034ae3e15f4081fdace7
Jason Adams <jmadams41@gmail.com>
2022-03-06 20:46:27 -0700

    indexing added

--- scrpit.js
+++ scrpit.js
@@ -1,7 +1,30 @@
-var startBtn = document.getElementById("start");
 
-function quizStart() {
-    startBtn.addEventListener("click", function () {
-        quizName.setAttribute("style", "display:none");
-        quizQuestionEl.setAttribute("style", "display: block");
-        controlsEl.setAttribute("style", "display: block;");
+
+// array of questions
+var quizQuestions = ["What is == in Js?", "What does ++ mean in Js?", "What does -- mean in Js?", "What does Boolean mean?"];
+
+// both incorrect and correct answers for all questions
+var buttonOneAnswers = ['super-equals', 'When you have to invite someone to a wedding it is funny how this is actually kinda correct', 'super negative like my attitude','true/false'];
+var buttonTwoAnswers = ['comparison operator','super positive', 'decrements the value', 'on/off'];
+var buttonThreeAnswers = ['I did not take notes on this', 'adds one increment to a value', 'ultra dumb', '1/0'];
+var buttonFourAnswers = ['Where are my socks', 'dead tree',  'wrong answer', 'all of the above technically',];
+
+
+//the correct answers to the questions
+
+var correctAnswers = ['comparison operator', 'adds one increment to a value', 'decrements the value','all of the above technically'];
+
+var index = 0;
+
+
+// question index
+
+var index = 0;
+
+// button index
+
+var buttonOneIndex = 0;
+var buttonTwoIndex = 0;
+var buttonThreeIndex = 0;
+var buttonFourIndex = 0;
+var correctAnswersIndex = 0;
