97bad6e5b57368cfb922c7dbf96b899f92fea7f8
Jason Adams <jmadams41@gmail.com>
2022-03-06 22:40:09 -0700

    start quiz and buttons

--- scrpit.js
+++ scrpit.js
@@ -1,4 +1,18 @@
+var startBtn = document.getElementById("start");
+var quizName = document.getElementById("quiz-name-div");
+var quizQuestionEl = document.getElementById("hidden-container");
+var controlsEl = document.getElementById("hidden-controls-container");
+var questTextEl = document.getElementById("question-text");
+var btn = document.createElement("button");
+var answerEl = document.getElementById("options");
 
+btn.className += "buttons";
+var btn2 = document.createElement("button");
+btn2.className += "buttons";
+let btn3 = document.createElement("button");
+btn3.className += "buttons";
+let btn4 = document.createElement("button");
+btn4.className += "buttons";
 
 // array of questions
 var quizQuestions = ["What is == in Js?", "What does ++ mean in Js?", "What does -- mean in Js?", "What does Boolean mean?"];
@@ -28,3 +42,34 @@ var buttonTwoIndex = 0;
 var buttonThreeIndex = 0;
 var buttonFourIndex = 0;
 var correctAnswersIndex = 0;
+
+// this makes the questions and answers appear when the start button is pressed
+
+
+function quizStart() {
+    startBtn.addEventListener("click", function () {
+        quizName.setAttribute("style", "display:none");
+        quizQuestionEl.setAttribute("style", "display: block");
+        controlsEl.setAttribute("style", "display: block;");
+        questTextEl.innerText = quizQuestions[0];
+
+        btn.textContent = buttonOneAnswers[0];
+        answerEl.appendChild(btn);
+
+        btn2.textContent = buttonTwoAnswers[0];
+        answerEl.appendChild(btn2);
+
+        btn3.textContent = buttonThreeAnswers[0];
+        answerEl.appendChild(btn3);
+
+        btn4.textContent = buttonFourAnswers[0];
+        answerEl.appendChild(btn4);
+
+
+
+
+
+
+        
+
+        quizStart();
